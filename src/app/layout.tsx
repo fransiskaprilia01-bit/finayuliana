d7e5257639397ea776be901da9904d8e8ffb025b
fransiskaprilia01-bit <fransiskaprilia01@gmail.com>
2025-12-19 17:53:18 +0700

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -42,6 +42,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <head>
+        <meta name="facebook-domain-verification" content="85ts6vhdgc23tza8rsutzxm925kqhy" />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased bg-background text-foreground`}
       >
